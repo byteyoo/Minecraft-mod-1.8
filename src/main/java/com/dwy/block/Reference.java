17eea7fce14b0994229269dabe4ffa70aaacbc15
Dong Woo Yoo <dyoo3@albany.edu>
2015-03-17 19:39:58 -0400

    added version 1.0

---
+++ src/main/java/com/dwy/block/Reference.java
@@ -0,0 +1,7 @@
+package com.dwy.block;
+
+public class Reference {
+    public static final String MOD_ID = "dwymod";
+    public static final String MOD_NAME = "Yoo Mod";
+    public static final String VERSION = "1.0";
+}
